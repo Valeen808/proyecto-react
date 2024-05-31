8826aced949fd4de816f508bfdd39965df3b686b
Valentina <valentinamancera056@gmail.com>
2024-05-31 17:34:51 -0500

    Fotos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 export function App () {
     return (
-        <article>
-            <header>
-                <img src="" />
-            </header>
-        </article>
+        <>
+            <TwitterFollowCard userName="Carla" name="Carla Garcia Martinez"/>
+            <TwitterFollowCard userName="Midudev" name="Midu Dev "/>
+            <TwitterFollowCard userName="Midu" name="Micky Mouse "/>
+        </>
     )
 }
     
